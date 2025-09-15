0c1b6fd51d2ddf27c519f1f845839a610bbc7af2
neo <source.fancy398@passinbox.com>
2025-09-15 17:50:46 -0500

    refactor force dark theme

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod radar;
 
 use clap::Parser;
 use context::Context;
-use egui::{Style, Visuals};
+use egui::Theme;
 use memflow::prelude::v1::*;
 
 #[derive(Parser, Debug)]
@@ -51,11 +51,7 @@ fn main() -> eframe::Result {
         "radar",
         native_options,
         Box::new(|cc| {
-            let style = Style {
-                visuals: Visuals::dark(),
-                ..Default::default()
-            };
-            cc.egui_ctx.set_style(style);
+            cc.egui_ctx.set_theme(Theme::Dark);
             egui_extras::install_image_loaders(&cc.egui_ctx);
             Ok(Box::new(Radar::new(context)))
         }),
